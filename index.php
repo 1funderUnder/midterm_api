96740691d8a0e35a219b93dc15115091f78f74fe
Laura Funderburk <l_funderburk@mail.fhsu.edu>
2025-03-25 15:12:37 -0500

    added text to index page

--- index.php
+++ index.php
@@ -9,15 +9,19 @@ if ($method === 'OPTIONS') {
     header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
     exit();
 
-    
+}  
 
-    echo '<pre>';
-    print_r(getenv('SITE_URL'));
-    echo '<br>';
-    print_r($_SERVER);
-    echo '</pre>';
+    echo '<h1>Welcome to My Midterm Page for INF653</h1>';
+    echo '<h2>By Laura Funderburk</h2>';
+    echo '<p>This is the main page. Here, you can access various APIs for quotes, categories, and authors.</p>';
+    echo '<p>You can interact with the API using the following endpoints:</p>';
     
-    phpinfo();   
-}
-?>    
+    echo '<ul>
+            <li> /api/quotes</li>
+            <li> /api/categories</li>
+            <li> /api/authors</li>
+          </ul>';
+    
+    echo '<p> Thanks for visiting!';
+    ?>  
 
